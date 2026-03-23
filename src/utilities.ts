b2c1c6006dacda97a4c61b44c55d1456abd7f481
Michael Levy <michaelfeehanlevy@gmail.com>
2026-03-23 20:11:30 +1300

    feat: add CellType enum and CellValue type

--- src/utilities.ts
+++ src/utilities.ts
@@ -12,3 +12,13 @@ export enum Rarity {
     RARE = 'RARE',
     EPIC = 'EPIC'
 }
+
+export enum CellType {
+    EMPTY = '0',
+    FOOD = 'f',
+    SPECIAL = 'p',
+    HAZARD = 'r',
+}
+
+// CellValue covers enum values plus snake colour/ID strings
+export type CellValue = CellType | string;
